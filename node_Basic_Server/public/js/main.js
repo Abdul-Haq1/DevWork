7e67ad26ba1111f3dabc6e3e14ad6f4f16470236
Abdul Haq <abdulhaqhk@gmail.com>
2024-10-15 16:53:33 +0530

    basic staructre of complete application is complete all the files connect to each other

---
+++ node_Basic_Server/public/js/main.js
@@ -0,0 +1,7 @@
+let paragraph = document.getElementById('pargraph')
+
+paragraph.addEventListener('click', changeColor)
+
+function changeColor() {
+    paragraph.style.color = 'black';
+}
--- node_Basic_Server/server.js
+++ node_Basic_Server/server.js
@@ -1,46 +1,63 @@
 const http = require('http');
 const path = require('path');
 const fs = require('fs');
+const { error } = require('console');
 
 const server = http.createServer((req, res) => {
 
-    // Build file path
-    let filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url);
-
-    // Extension of file
-    let extname = path.extname(filePath)
-
-    // intial content type
-    let contentType = 'text/html';
-
-    // check ext and set content type
-    switch (extname) {
-        case '.js':
-            contentType = 'text/javascript';
-            break;
-        case '.css':
-            contentType = 'text/css'
-            break;
+    if (req.url === '/') {
+        fs.readFile(path.join(__dirname, 'public', 'index.html'),
+            (err, content) => {
+                if (err) throw err;
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.end(content, 'utf-8')
+            }
+        )
+    } else if (req.url === '/about') {
+        fs.readFile(path.join(__dirname, 'public', 'about.html'),
+            (err, content) => {
+                if (err) throw err;
+                res.writeHead(200, { 'Content-Type': 'text/html' })
+                res.end(content)
+            }
+        )
+    } else if (req.url === '/css/index.css') {
+        fs.readFile(path.join(__dirname, 'public', 'css/index.css'),
+            (err, content) => {
+                if (err) throw err;
+                res.writeHead(200, { 'Content-Type': 'text/css' })
+                res.end(content, 'utf-8')
+            }
+        )
+    } else if (req.url === '/js/main.js') {
+        fs.readFile(path.join(__dirname, 'public', req.url),
+            (err, content) => {
+                if (err) throw err;
+                res.writeHead(200, { 'Conent-Type': 'text/javaScript' })
+                res.end(content, 'utf-8')
+            }
+        )
     }
 
-    fs.readFile(filePath, (err, content) => {
-        if (err) {
-            if (err.code == ENONET) {
-                // page not found
-                fs.readfile(path.join('__dirname', 'public', 'notFound.html'),
-                    (err, content) => {
-                        res.writeHead(200, { 'Content-Type': 'text/html' });
-                        res.end(content, 'utf8');
-                    })
-            } else {
-                res.writeHead(500);
-                res.end(`server error: ${err.code}`);
-            }
-        } else {
-            res.writeHead(200, { 'Content-Type': contentType })
-            res.end(content, 'utf8');
-        }
-    })
+
+    // fs.readFile(`${filePath}.html`, (err, content) => {
+    //     if (err) {
+    //         if (err.code == 'ENOENT') {
+    //             // page not found
+    //             fs.readFile(path.join(__dirname, 'public', 'notFound.html'),
+    //                 (err, content) => {
+    //                     res.writeHead(200, { 'Content-Type': 'text/html' });
+    //                     res.end(content, 'utf8');
+    //                 })
+    //         } else {
+    //             res.writeHead(500);
+    //             res.end(`server error: ${err.code}`);
+    //         }
+    //     } else {
+    //         res.writeHead(200, { 'Content-Type': contentType })
+    //         res.end(content, 'utf8');
+    //     }
+    // })
 
 
 })
